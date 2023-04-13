d4d49c59c1acca769beebb25348476179cfb8309
Romaman007 <72684401+Romaman007@users.noreply.github.com>
2023-04-14 01:27:12 +0700

    Update sql_func.py

--- in_py/sql_func.py
+++ in_py/sql_func.py
@@ -3,7 +3,7 @@ import sqlite3 as sq
 
 def sql_start():
     global base, cur
-    base = sq.connect(r'/post.db')
+    base = sq.connect(r'..\post.db')
     cur = base.cursor()
     if base:
         print('DBase connected!')
