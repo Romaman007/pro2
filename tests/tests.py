9d206c81cb9255327de92e39e7ee085339a71cdf
Sevostyan <sevostyanovd53@gmail.com>
2023-04-15 18:23:30 +0300

    tests added

--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-import sql_func
-import pandas as pd
-
-
-def test_wall():
-    sql_func.sql_start()
-    df = pd.read_sql_query("SELECT * from priznavashki", sql_func.base)
-    assert len(df) == 40090
