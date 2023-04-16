ca49b83ef16665a526927d8fec303b15f7563d8a
Sevostyan <sevostyanovd53@gmail.com>
2023-04-16 23:07:41 +0300

    final tests

--- in_py/tests.py
+++ in_py/tests.py
@@ -25,3 +25,26 @@ def test_users_main():
     df = pd.read_sql_query("SELECT * from users_main", sql_func.base)
     assert len(df) == 461
 
+
+def test_id_comments():
+    sql_func.sql_start()
+    df = pd.read_sql_query("SELECT * from all_comments WHERE user_id == 2340546", sql_func.base)
+    assert len(df) == 3
+
+
+def test_id_likes():
+    sql_func.sql_start()
+    df = pd.read_sql_query("SELECT * from likes_comments WHERE user_id == 560156454", sql_func.base)
+    assert len(df) == 1
+
+
+def test_id_priznavashki_anon():
+    sql_func.sql_start()
+    df = pd.read_sql_query("SELECT * from priznavashki WHERE user_id == -59518047", sql_func.base)
+    assert len(df) == 35585
+
+
+def test_id_users_main():
+    sql_func.sql_start()
+    df = pd.read_sql_query("SELECT * from users_main WHERE user_id == -50260527", sql_func.base)
+    assert len(df) == 86
